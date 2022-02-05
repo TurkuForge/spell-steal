977363fb19eb17fadbd8454272d98ee34a6bc182
Andreas Näsman <andreas.nasman@hibox.tv>
2022-02-05 22:07:22 +0200

    Add Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <img alt="Vue logo" src="./assets/logo.png" class="mx-auto" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
