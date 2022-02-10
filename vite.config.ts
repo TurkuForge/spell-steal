3fce5cb023ece323ebb6a91aec44fa592088addc
Andreas Näsman <andreas.nasman@hibox.tv>
2022-02-10 11:39:03 +0200

    Implement Reactivity Transform

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue({ reactivityTransform: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
